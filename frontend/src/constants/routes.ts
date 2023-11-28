c7ec014b993f66d4beaf44b5b5be240217d17188
Marco <marcodavidtoledo@gmail.com>
2023-11-28 12:34:41 -0400

    first commit

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,23 @@
+export enum PublicRoutes {
+  LOGIN = "/login",
+  REGISTER = "/register",
+  LANDING = "/landing",
+}
+
+export enum PrivateRoutes {
+  PRIVATE = "/private",
+  DASHBOARD = "/dashboard",
+  HOME = "/home",
+  USERS = "/users",
+  PHOTOGRAPHERS = "/photographers",
+  ORGANIZERS = "/organizers",
+  CLIENTS = "/clients",
+  EVENTS = "/events",
+  PHOTOS = "/photos",
+  PHOTOSSALE = "/photos-sale",
+  CART = "/cart",
+  MYPHOTOS = "/my-photos",
+  FACTURES = "/facturas",
+}
+
+export const baseUrl = "http://192.168.0.202:3000/";
